9b9383d5253e5676362ba2b3a52fae3d41b54160
Harry <garikhabuzov@gmail.com>
2020-03-27 13:03:35 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict'
+
+
+
+alert(1);
+var number = 5;
+console.log(4/0);
+console.log('string'*9);
+
+let array = ['plum.png', 'orange.jpg',];
+console.log(array[0]);
+
+let answer = confirm("Are you here?");
+console.log(answer);
