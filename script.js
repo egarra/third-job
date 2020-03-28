dd2b3a30cd8c5a43c3c673f8a8522be4ced6e4f3
Harry <garikhabuzov@gmail.com>
2020-03-28 14:11:44 +0200

    first job

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 'use strict'
 
+let money = prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
 
-alert(1);
-var number = 5;
-console.log(4/0);
-console.log('string'*9);
+let firstQuestion = prompt("Введите обязательную статью расходов в этом месяце", "");
+let secondQuestion = prompt("Во сколько обойдется?", "");
+let thirdQuestion = prompt("Введите необязательную статью расходов в этом месяце", "");
+let fourthQuestion = prompt("Во сколько обойдется?", "");
 
-let array = ['plum.png', 'orange.jpg',];
-console.log(array[0]);
+appData.expenses.firstQuestion = secondQuestion;
+appData.expenses.thirdQuestion = fourthQuestion;
 
-let answer = confirm("Are you here?");
-console.log(answer);
+alert(appData.budget / 30);
+alert(key in appData);
